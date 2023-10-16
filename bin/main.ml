36873da4db3abe46e0f5c90649f23602449fbb7d
Matti Eiden <snaipperi@gmail.com>
2023-10-16 22:06:20 +0300

    Split task to separate module

--- bin/main.ml
+++ bin/main.ml
@@ -1,54 +1,16 @@
+module Task = Wq.Task
 
+let connect () =
+  Caqti_lwt.connect @@ Uri.of_string "postgresql://wq@localhost/wq"
 
-let my_query =
-  [%rapper
-    get_opt
-      {sql|
-      WITH cte AS (
-        SELECT 
-          id, 
-          title, 
-          priority, 
-          deadline, 
-          created_at,
-          pow(pow(2, priority), 2) as p_factor
-        FROM task 
-      )
-      SELECT 
-        @int{id}, 
-        @string{title}, 
-        @int{priority}, 
-        @string?{deadline}, 
-        p_factor * EXTRACT(EPOCH FROM (NOW() - created_at)) / 86400.0 +
-          CASE
-            WHEN deadline IS NOT NULL THEN 
-              p_factor / (0.01 * exp(
-                EXTRACT(EPOCH FROM (deadline - NOW())) / 86400.0)
-              )
-            ELSE 0
-          END 
-          AS @float{score}
-      FROM cte
-      ORDER BY score ASC
-      LIMIT 1
-      |sql}]
-
-
-let connect () = 
-  Caqti_lwt.connect @@ (Uri.of_string "postgresql://wq@localhost/wq")
-
-
-let exec_query () = 
+let exec_query () =
   let ( let* ) = Lwt_result.bind in
   let* conn = connect () in
-  my_query () conn
+  Task.list_with_score () conn
 
 let () =
   match Lwt_main.run @@ exec_query () with
   | Error err ->
       print_endline "Oops, we encountered an error!";
       print_endline (Caqti_error.show err)
-  | Ok (Some (id, title, _, _, score)) -> Printf.printf "Returned row id %i with title \"%s\" and score %f\n" id title score
-  | Ok None -> Printf.printf "Hello world\n" 
-  
-
+  | Ok rows -> List.iter Task.print_task rows
---
+++ lib/task.ml
@@ -0,0 +1,47 @@
+type model =
+  { id : int;
+    title: string;
+    priority: int;
+    deadline: Ptime.t option;
+    score: float;
+  }
+
+  let print_task (row : model) = 
+    match row with 
+      | {id; title; score; _} -> Printf.printf "Returned row id %i with title \"%s\" and score %f\n" id title score
+    
+  
+
+  let list_with_score =
+    [%rapper
+      get_many
+        {sql|
+        WITH cte AS (
+          SELECT 
+            id, 
+            title, 
+            priority, 
+            deadline, 
+            created_at,
+            pow(pow(2, priority), 2) as p_factor,
+            extract(epoch from (now() - created_at)) / 86400.0 as age,
+            extract(epoch from (deadline - NOW())) / 86400.0 as ttl
+          FROM task 
+        )
+        SELECT 
+          @int{id}, 
+          @string{title}, 
+          @int{priority}, 
+          @ptime?{deadline}, 
+          p_factor * age +
+            CASE
+              WHEN ttl IS NOT NULL THEN 
+                p_factor / (0.01 * exp(ttl))
+              ELSE 0
+            END 
+            AS @float{score}
+        FROM cte
+        ORDER BY score DESC
+        |sql}
+        record_out]
+  
