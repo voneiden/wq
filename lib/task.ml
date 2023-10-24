4baa3788da7943521807db34d7bd8a30306f9b90
Matti Eiden <snaipperi@gmail.com>
2023-10-24 23:33:59 +0300

    Refactor from CalendarLib to Timedesc

--- lib/task.ml
+++ lib/task.ml
@@ -1,91 +1,116 @@
-module Calendar = CalendarLib.Calendar
-module Date = CalendarLib.Date
-module Period = CalendarLib.Period
-module Time = CalendarLib.Time
-module Printer = CalendarLib.Printer
+let tz = Timedesc.Time_zone.make_exn "Europe/Helsinki"
+(* TODO customizable *)
+
+module Timestamp = Timedesc.Timestamp
+module Span = Timedesc.Span
+module Time = Timedesc.Time
+module Date = Timedesc.Date
 
 type db_task = {
   id : int;
   title : string;
   priority : int;
-  deadline : Calendar.t option;
-  estimate : Ptime.span option;
+  deadline : Timestamp.t option;
+  estimate : Span.t option;
   score : float;
   closed : bool;
 }
 
-type task = { db_task : db_task; start_time : Calendar.t option }
+let make_timestamp year month day hour minute second =
+  Timedesc.to_timestamp_single
+  @@ Timedesc.make_exn ~tz ~year ~month ~day ~hour ~minute ~second ()
 
-let ptime_span_to_calendar_period (span : Ptime.span option) =
-  Option.bind span (fun span -> Ptime.Span.to_int_s span)
-  |> Option.map (fun seconds -> Calendar.Period.make 0 0 0 0 0 seconds)
+let make_time hour minute second = Time.make_exn ~hour ~minute ~second ()
+
+let make_time_span hour minute second =
+  Time.to_span @@ make_time hour minute second
+
+let to_db_task ~id ~title ~priority ~deadline ~estimate ~closed ~score =
+  {
+    id;
+    title;
+    priority;
+    deadline = Option.map Timedesc.Utils.timestamp_of_ptime deadline;
+    estimate = Option.map Timedesc.Utils.span_of_ptime_span estimate;
+    score;
+    closed;
+  }
+
+type task = { db_task : db_task; start_time : Timestamp.t option }
+
+let timestamp_add_days days (timestamp : Timestamp.t) =
+  let datetime = Timedesc.of_timestamp_exn ~tz_of_date_time:tz timestamp in
+  Timedesc.of_date_and_time_exn
+    (Date.add ~days (Timedesc.date datetime))
+    (Timedesc.time datetime)
+  |> Timedesc.to_timestamp_single
+
+let set_timestamp_time (time : Time.t) (timestamp : Timestamp.t) =
+  let datetime = Timedesc.of_timestamp_exn ~tz_of_date_time:tz timestamp in
+  Timedesc.of_date_and_time_exn ~tz (Timedesc.date datetime) time
+  |> Timedesc.to_timestamp_single
 
 let%test _ =
-  ptime_span_to_calendar_period (Some (Ptime.Span.of_int_s 100))
-  = Some (Calendar.Period.make 0 0 0 0 1 40)
+  set_timestamp_time
+    (Time.make_exn ~hour:16 ~minute:59 ~second:45 ())
+    (Timedesc.make_exn ~tz ~year:2023 ~month:10 ~day:19 ~hour:8 ~minute:0
+       ~second:0 ()
+    |> Timedesc.to_timestamp_single)
+  = (Timedesc.make_exn ~year:2023 ~month:10 ~day:19 ~hour:16 ~minute:59
+       ~second:45 ()
+    |> Timedesc.to_timestamp_single)
 
-let set_calendar_time (time : Calendar.Time.t) (calendar : Calendar.t) =
-  Calendar.make (Calendar.year calendar)
-    (Date.int_of_month @@ Calendar.month calendar)
-    (Calendar.day_of_month calendar)
-    (Time.hour time) (Time.minute time) (Time.second time)
-
-let%test _ =
-  set_calendar_time (Time.make 16 59 45) (Calendar.make 2023 10 19 8 0 0)
-  = Calendar.make 2023 10 19 16 59 45
-
-(* Given a day start and end times (eg. 8-16), subtract work time from a calendar *)
 let rec subtract_work_time (day_start : Time.t) (day_end : Time.t)
-    (time : Calendar.Period.t) (moment : Calendar.t) =
+    (time : Span.t) (timestamp : Timestamp.t) =
   let recurse = subtract_work_time day_start day_end in
-  match Calendar.day_of_week moment with
-  | Calendar.Sun ->
-      Calendar.rem moment (Calendar.Period.day 2)
-      |> set_calendar_time day_end |> recurse time
-  | Calendar.Sat ->
-      Calendar.rem moment (Calendar.Period.day 1)
-      |> set_calendar_time day_end |> recurse time
+  let dt = Timedesc.of_timestamp_exn ~tz_of_date_time:tz timestamp in
+  match Timedesc.weekday dt with
+  | `Sun ->
+      timestamp_add_days (-2) timestamp
+      |> set_timestamp_time day_end |> recurse time
+  | `Sat ->
+      timestamp_add_days (-1) timestamp
+      |> set_timestamp_time day_end |> recurse time
   | _ ->
-      let calendar_day_start = set_calendar_time day_start moment
-      and calendar_day_end = set_calendar_time day_end moment in
-      if moment > calendar_day_end then
-        set_calendar_time day_end moment |> recurse time
-      else if moment <= calendar_day_start then
-        Calendar.rem moment (Calendar.Period.day 1)
-        |> set_calendar_time day_end |> recurse time
+      let day_start_timestamp = set_timestamp_time day_start timestamp
+      and day_end_timestamp = set_timestamp_time day_end timestamp in
+      if timestamp > day_end_timestamp then
+        set_timestamp_time day_end timestamp |> recurse time
+      else if timestamp <= day_start_timestamp then
+        timestamp_add_days (-1) timestamp
+        |> set_timestamp_time day_end |> recurse time
       else
         let day_max_work_time =
-          Calendar.sub calendar_day_end calendar_day_start
+          Span.sub day_end_timestamp day_start_timestamp
         in
         let actual_day_work_time = min day_max_work_time time in
-        let remaining_time = Calendar.Period.sub time actual_day_work_time
-        and new_moment = Calendar.rem moment actual_day_work_time in
-        if remaining_time = Calendar.Period.empty then new_moment
+        let remaining_time = Span.sub time actual_day_work_time
+        and new_moment = Span.sub timestamp actual_day_work_time in
+        if remaining_time = Span.zero then new_moment
         else recurse remaining_time new_moment
 
 let%test _ =
-  subtract_work_time (Time.make 8 0 0) (Time.make 16 0 0)
-    (Calendar.Period.make 0 0 0 2 0 0)
-    (Calendar.make 2023 10 15 23 0 0)
-  = Calendar.make 2023 10 13 14 0 0
+  subtract_work_time (make_time 8 0 0) (make_time 16 0 0) (make_time_span 2 0 0)
+    (make_timestamp 2023 10 15 23 0 0)
+  = make_timestamp 2023 10 13 14 0 0
 
 let%test _ =
-  subtract_work_time (Time.make 8 0 0) (Time.make 16 0 0)
-    (Calendar.Period.make 0 0 0 12 0 0)
-    (Calendar.make 2023 10 15 23 0 0)
-  = Calendar.make 2023 10 12 12 0 0
+  subtract_work_time (make_time 8 0 0) (make_time 16 0 0)
+    (make_time_span 12 0 0)
+    (make_timestamp 2023 10 15 23 0 0)
+  = make_timestamp 2023 10 12 12 0 0
 
-let deadline_or_override (deadline : Calendar.t option)
-    (override : Calendar.t option) =
+let deadline_or_override (deadline : Timestamp.t option)
+    (override : Timestamp.t option) =
   match (deadline, override) with
   | Some deadline, Some override -> Some (min deadline override)
   | Some deadline, None -> Some deadline
   | _ -> None
 
-let determine_start_time (end_time : Calendar.t) : Calendar.t =
-  subtract_work_time (Time.make 8 0 0)
-    (Time.make 16 0 0) (* TODO make configurable*)
-    (Calendar.Period.make 0 0 0 1 0 0) (* TODO: make dynamic*)
+let determine_start_time (end_time : Timestamp.t) : Timestamp.t =
+  subtract_work_time (make_time 8 0 0)
+    (make_time 16 0 0) (* TODO make configurable*)
+    (make_time_span 1 0 0) (* TODO: make dynamic*)
     end_time
 
 let sort_optional (getter : 'a -> 'b option) (compare : 'b -> 'b -> int)
@@ -100,13 +125,13 @@ let select_earliest_start_time (tasks : task list) : task option =
   let start_tasks =
     tasks
     |> List.filter (fun task -> Option.is_some task.start_time)
-    |> List.sort (sort_optional (fun task -> task.start_time) Calendar.compare)
+    |> List.sort (sort_optional (fun task -> task.start_time) Timestamp.compare)
   in
   match start_tasks with hd :: _ -> Some hd | _ -> None
 
 let task_factory (hour : int) =
   {
-    start_time = Some (Calendar.make 2023 10 20 hour 0 0);
+    start_time = Some (make_timestamp 2023 10 20 hour 0 0);
     db_task =
       {
         score = 0.0;
@@ -126,7 +151,8 @@ let%test _ =
     [ task_factory 12; task_factory 10; task_factory 14 ]
   = Some (task_factory 10)
 
-let select_most_important (now : Calendar.t) (tasks : task list) : task option =
+let select_most_important (now : Timestamp.t) (tasks : task list) : task option
+    =
   let earliest_start_time = select_earliest_start_time tasks in
   match earliest_start_time with
   | Some { start_time = Some start_time; _ } when start_time < now ->
@@ -144,7 +170,7 @@ let select_most_important (now : Calendar.t) (tasks : task list) : task option =
 let to_tasks (db_tasks : db_task list) : task list =
   db_tasks
   |> List.sort
-       (sort_optional (fun db_task -> db_task.deadline) Calendar.compare)
+       (sort_optional (fun db_task -> db_task.deadline) Timestamp.compare)
   |> List.rev
   |> List.fold_left_map
        (fun previous_start db_task ->
@@ -159,7 +185,7 @@ let to_tasks (db_tasks : db_task list) : task list =
 let db_task_factory (hour : int) =
   {
     id = 1;
-    deadline = Some (Calendar.make 2023 10 20 hour 0 0);
+    deadline = Some (make_timestamp 2023 10 20 hour 0 0);
     closed = false;
     estimate = None;
     title = "";
@@ -173,24 +199,29 @@ let%test _ =
   to_tasks [ db_task_factory 10 ]
   = [
       {
-        start_time = Some (Calendar.make 2023 10 20 9 0 0);
+        start_time = Some (make_timestamp 2023 10 20 9 0 0);
         db_task = db_task_factory 10;
       };
     ]
 
 let print_most_important (tasks : task list) =
-  match select_most_important (Calendar.now ()) tasks with
+  match select_most_important (Timestamp.now ()) tasks with
   | Some { db_task = { id; title; _ }; _ } ->
       Printf.printf "The most important task (id=%i): %s" id title
   | None -> Printf.printf "Nothing to do!"
 
 let print_task (row : task) =
   match row with
-  | { db_task = { id; title; score; _ }; start_time = Some start_time } ->
+  | {
+   db_task = { id; title; score; deadline = Some deadline; _ };
+   start_time = Some start_time;
+  } ->
       Printf.printf
-        "Returned row id %i with title \"%s\" and score %f -- start at %s\n" id
-        title score
-        (Printer.CalendarPrinter.to_string start_time)
+        "Returned row id %i with title \"%s\" and score %f -- start at %s \
+         (deadline at %s)\n"
+        id title score
+        (Timestamp.to_string start_time)
+        (Timestamp.to_string deadline)
   | { db_task = { id; title; score; _ }; _ } ->
       Printf.printf "Returned row id %i with title \"%s\" and score %f\n" id
         title score
@@ -200,7 +231,7 @@ let insert =
     execute
       {sql|
         INSERT INTO task(title, priority, deadline, estimate)
-        VALUES (%string{title}, %int{priority}, %ctime?{deadline}, %ptime_span?{estimate})
+        VALUES (%string{title}, %int{priority}, %ptime?{deadline}, %ptime_span?{estimate})
       |sql}]
 
 let list_with_score =
@@ -226,9 +257,9 @@ let list_with_score =
           @int{id}, 
           @string{title}, 
           @int{priority}, 
-          @ctime?{deadline}, 
-          @bool{closed},
+          @ptime?{deadline}, 
           @ptime_span?{estimate},
+          @bool{closed},
           p_factor * age +
             CASE
               WHEN ttl IS NOT NULL THEN 
@@ -239,4 +270,5 @@ let list_with_score =
         FROM cte
         ORDER BY score DESC
         |sql}
-      record_out]
+      function_out]
+    to_db_task
